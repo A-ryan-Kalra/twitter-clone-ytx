aafa3f3adb94673477fe74152958e1a453b50b9a
Aaryan Kalra <aryan.smart832@gmail.com>
2023-08-11 19:25:57 +0530

    sidebar on progress

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,12 +1,53 @@
 import Image from "next/image";
+import SidebarLink from "./SidebarLink";
+import { HomeIcon } from "@heroicons/react/24/solid";
+
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardDocumentListIcon,
+  UserIcon,
+  EllipsisHorizontalCircleIcon,
+  EllipsisHorizontalIcon,
+} from "@heroicons/react/24/outline";
 function Sidebar() {
   return (
-    <div className=" hidden sm:flex flex-col items-center xl:items-start  xl:w-[340px]  p-2 fixed h-full  ">
+    <div className=" hidden sm:flex flex-col items-center xl:items-start  xl:w-[340px]   p-2 fixed h-full  ">
       <div
-        className="flex items-center justify-center w-14 h-14 hoverAnimation
+        className="flex items-center justify-center  w-14 h-14 hoverAnimation
         p-0 xl:ml-24 "
       >
         <Image src="https://rb.gy/ogau5a" width={30} className="" height={30} />
+      </div>
+      <div className="space-y-2.5 mt-4 mb-3 xl:ml-24">
+        <SidebarLink text="Home" Icon={HomeIcon} active />
+        <SidebarLink text="Explore" Icon={HashtagIcon} />
+        <SidebarLink text="Notifications" Icon={BellIcon} />
+        <SidebarLink text="Messages" Icon={InboxIcon} />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarLink text="Lists" Icon={ClipboardDocumentListIcon} />
+        <SidebarLink text="Profile" Icon={UserIcon} />
+        <SidebarLink text="More" Icon={EllipsisHorizontalCircleIcon} />
+      </div>
+      <button
+        className="hidden xl:ml-24 xl:inline ml-auto rounded-full text-lg font-bold shadow-md hover:bg-[#1a8cd8]
+       w-48 h-[46px] bg-[#1d9bf0] text-white"
+      >
+        Tweet
+      </button>
+      <div className="text-[#d9d9d9] flex items-center justify-center hoverAnimation xl:ml-auto xl:-mr-2.5 mt-auto">
+        <img
+          src="https://chat.openai.com/apple-touch-icon.png"
+          className="h-10 w-10 rounded-full xl:mr-2.5"
+          alt=""
+        />
+        <div className="hidden xl:inline leading-5">
+          <h4>firebase 1999</h4>
+          <p className="text-[#6e767d]">@firebase1999</p>
+        </div>
+        <EllipsisHorizontalIcon className="h-5 hidden xl:inline ml-10" />
       </div>
     </div>
   );
