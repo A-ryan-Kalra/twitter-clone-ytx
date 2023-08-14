c72274f73e52a0a0b6b6bfe29403b02b72ea30b0
Aaryan Kalra <aryan.smart832@gmail.com>
2023-08-14 17:18:24 +0530

    done with login page

---
+++ components/Login.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { signIn } from "next-auth/react";
+
+function Login({ providers }) {
+  //   console.log(Object.values(providers));
+  return (
+    <div className="text-white flex flex-col items-center justify-center  h-screen">
+      <Image
+        src={"https://rb.gy/ogau5a"}
+        width={150}
+        height={150}
+        objectFit="contain"
+      />
+      <div>
+        {Object.values(providers).map((provider, index) => (
+          <div key={index}>
+            <button
+              class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              <span class="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+              <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
+                Sign in with {provider.name}
+              </span>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,19 @@ import Head from "next/head";
 import Image from "next/image";
 import Sidebar from "../../components/Sidebar";
 import Feed from "../../components/Feed";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import Login from "../../components/Login";
+import { data } from "autoprefixer";
 
-export default function Home() {
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session, status } = useSession();
+
+  console.log(status);
+  console.log(session);
+  if (!session) return <Login providers={providers} />;
+  // console.log(providers);
   return (
     <div className="">
       <Head>
@@ -16,4 +27,27 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  // const session = await getServerSession(context.req, context.res, authOptions);
+
+  const trendingResults = await fetch("https://www.jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://www.jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+
+  const providers = await getProviders();
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      session,
+      providers,
+    },
+  };
 }
