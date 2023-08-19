5b7235c41ee8407ff3a675facb4fea165fdffd50
Aaryan Kalra <aryan.smart832@gmail.com>
2023-08-19 12:06:26 +0530

    resolved emoji not being show error

--- components/Input.js
+++ components/Input.js
@@ -40,14 +40,14 @@ function Input() {
       setSelectedFile(readerEvent.target.result);
     };
   };
-  const addEmoji = (e) => {
+  const addEmoji = async (e) => {
     // let sym = e.unified.split(" ");
     // let codeArray = [];
     // sym.forEach((el) => codeArray.push("0x" + el));
     // console.log(codeArray);
     // let emoji = String.fromCodePoint(codeArray);
     // console.log(emoji);
-    setInput(input + e.native);
+    setInput(input + (await e.native));
   };
 
   const sendPost = async () => {
