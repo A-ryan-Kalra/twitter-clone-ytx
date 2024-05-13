9ed29e4414ac702e98575b694de78db94b5e2beb
Aaryan Kalra <aryan.smart832@gmail.com>
2024-05-14 00:06:29 +0530

    wwasd

--- components/Input.js
+++ components/Input.js
@@ -35,6 +35,15 @@ function Input() {
     if (e.target.files[0]) {
       reader.readAsDataURL(e.target.files[0]);
     }
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
 
     // console.log(e.target.files[0] + " e.target.files[0]");
     reader.onload = (readerEvent) => {
