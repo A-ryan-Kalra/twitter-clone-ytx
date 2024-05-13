0dd4ae3b9005dfa61d5f288d9701b2189da83c4a
Aaryan Kalra <aryan.smart832@gmail.com>
2024-05-14 02:29:43 +0530

    okay2y

--- components/Input.js
+++ components/Input.js
@@ -45,9 +45,6 @@ function Input() {
     if (e.target.files[0]) {
       reader.readAsDataURL(e.target.files[0]);
     }
-    if (e.target.files[0]) {
-      reader.readAsDataURL(e.target.files[0]);
-    }
 
     // console.log(e.target.files[0] + " e.target.files[0]");
     reader.onload = (readerEvent) => {
