078059b3cabe43926872770e562d98fb94941606
Aaryan Kalra <aryan.smart832@gmail.com>
2024-05-15 02:14:54 +0530

    main merge11

--- components/Input.js
+++ components/Input.js
@@ -35,6 +35,10 @@ function Input() {
     if (e.target.files[0]) {
       reader.readAsDataURL(e.target.files[0]);
     }
+    if (e.target.files[0]) {
+      console.log("main merge11");
+    }
+
     // console.log(e.target.files[0] + " e.target.files[0]");
     reader.onload = (readerEvent) => {
       // console.log(readerEvent.target.result + " readerEvent.target.result");
