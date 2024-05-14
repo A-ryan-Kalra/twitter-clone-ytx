2b347e7c74de0758f48386da5ac3e26210d648d1
Aaryan Kalra <aryan.smart832@gmail.com>
2024-05-15 02:12:30 +0530

    okay merge1

--- components/Input.js
+++ components/Input.js
@@ -37,13 +37,7 @@ function Input() {
     }
 
     if (e.target.files[0]) {
-      reader.readAsDataURL(e.target.files[0]);
-    }
-    if (e.target.files[0]) {
-      reader.readAsDataURL(e.target.files[0]);
-    }
-    if (e.target.files[0]) {
-      reader.readAsDataURL(e.target.files[0]);
+      console.log("okay merge1");
     }
 
     // console.log(e.target.files[0] + " e.target.files[0]");
