017fc244f4c0900c713d8603b01a7a8ff3e9edf7
Aaryan Kalra <aryan.smart832@gmail.com>
2024-06-01 00:20:53 +0530

    added a tag v1

--- components/Input.js
+++ components/Input.js
@@ -35,6 +35,7 @@ function Input() {
     if (e.target.files[0]) {
       reader.readAsDataURL(e.target.files[0]);
     }
+    console.log("this is master branch along with new tag v1.0");
 
     // console.log(e.target.files[0] + " e.target.files[0]");
     reader.onload = (readerEvent) => {
