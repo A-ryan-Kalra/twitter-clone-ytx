e73234b78d0a96869961ccb26f4ea7d0f124aefb
Aaryan Kalra <aryan.smart832@gmail.com>
2024-06-01 00:24:53 +0530

    add one more extra console log

--- components/Input.js
+++ components/Input.js
@@ -37,6 +37,7 @@ function Input() {
     }
     console.log("this is master branch along with new tag v1.0");
     console.log("this is master branch along with new tag v1.0");
+    console.log("this is master branch along with new tag v1.0");
 
     // console.log(e.target.files[0] + " e.target.files[0]");
     reader.onload = (readerEvent) => {
